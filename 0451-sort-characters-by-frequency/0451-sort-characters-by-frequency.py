1b5445ccb1984bfc983706f8c8d467571a4b4844
Priyanshu24-max <priyanshubhatt0024@gmail.com>
2026-09-12 00:28:55 +0530

    Time: 11 ms (54.48%), Space: 20.1 MB (64.16%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -4,10 +4,10 @@ class Solution:
 
         for x in s:
             count[x]=count.get(x,0)+1
-        result=dict(sorted(count.items(),key=itemgetter(1),reverse=True)) #sort the dict in decending order according to the value
+        result=sorted(count.items(),key=itemgetter(1),reverse=True) #sort the dict in decending order according to the value and itemgetter(1) means take index 1 of each tuple, i.e. take the frequency.
         new=[] #to store final string
 
-        for key,value in result.items():
+        for key,value in result:
             new.append(key*value) #append key*value means if key:e and value:2 then append "ee"
         return "".join(new)
         
